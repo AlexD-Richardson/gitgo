7ba19710e6a0b61ece0db2157ee7b323d4ed0728
Alex Richardson <richardson.d.alex@gmail.com>
2020-02-01 16:51:34 -0500

    Added more to the main func

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	flag "github.com/ogier/pflag"
 )
 
-// flags
+// These be the flags ya looking for
 var (
 	user string
 )
 
 func main() {
+	// parsing ya flags
 	flag.Parse()
+
+	//if the user does not supply the necessary flags, just print usage
+	// TODO put this into its own func
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	users := strings.Split(user, ",")
+	fmt.Printf("Searching user(s): %s\n", users)
 }
 
 func init() {
