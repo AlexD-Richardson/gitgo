04e5ea0fb824cf12f0f7b65b7d05b6b89c060f9c
Alex Richardson <richardson.d.alex@gmail.com>
2020-03-03 17:53:59 -0500

    Typo :(

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 		fmt.Printf("Name: %s ", result.Name)
 		fmt.Printf("Email: %s ", result.Email)
 		fmt.Printf("Bio: %s ", result.Bio)
-		fmt.Printf("Public Repo: %i ", result.PublicRepos)
+		fmt.Printf("Public Repo: %d ", result.PublicRepos)
 		fmt.Println("")
 	}
 }
